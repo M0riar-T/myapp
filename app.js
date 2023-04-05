61db37d52cd4f5d7eac83f43d8dd366f7ca42e0c
Jenish Patel <dark.pearl007@gmail.com>
2023-04-05 10:30:36 +0530

    added time stamp

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ let port = 3003;
 app.use(express.json());
 app.get('/stt', (req, res) => {
   res.send({"message":"Hello world from Kubernetese...."});
+  var currentdate = new Date(); 
+  var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ":"
+                + currentdate.getMilliseconds();
+  console.log(`${datetime} vCash app1 was called on port ${port}`);
 });
 
 app.listen(port, () => {
